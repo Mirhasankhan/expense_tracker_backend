e203298d4f0ea223f57bb8e589bb08bb2a6d0203
mirhasankhan <mirhasan000034@gmail.com>
2025-08-15 19:32:19 +0600

    initial commit

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,12 @@
+import { Types, Document } from "mongoose";
+
+export interface TUser extends Document {
+  _id: Types.ObjectId;
+  fullName: string;
+  email: string;
+  password: string;
+  otp: string;
+  role: string;
+  expiresAt: Date;
+  
+}
---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,65 @@
+import { model, Schema } from "mongoose";
+import { TUser } from "./user.interface";
+
+const userSchema = new Schema<TUser>(
+  {
+    fullName: { type: String, required: [true, "Fullname is required"] },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      trim: true,
+      match: [
+        /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    password: { type: String, required: [true, "Password is required"] },
+    role: {
+      type: String,
+      default: "User",
+      enum: ["User", "Admin"],
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+export const User = model<TUser>("User", userSchema);
+
+const pendingUserSchema = new Schema<TUser>(
+  {
+    fullName: { type: String, required: [true, "Fullname is required"] },
+    email: { type: String, required: [true, "Email is required"] },
+    password: { type: String, required: [true, "Password is required"] },
+    role: {
+      type: String,
+      default: "User",
+      enum: ["User", "Admin"],
+    },
+    otp: { type: String },
+    expiresAt: { type: Date, required: true },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+export const PendingUser = model<TUser>("PendingUser", pendingUserSchema);
+
+const otpSchema = new Schema<TUser>(
+  {
+    email: { type: String, required: [true, "Email is required"] },
+    otp: { type: String },
+    expiresAt: { type: Date, required: true },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+export const Otp = model<TUser>("Otp", otpSchema);
